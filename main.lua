a2bc233146be5448dcf469f4181fdb4f7aff9a68
Lucas Schraier <lucas.schraier@shipsigma.com>
2024-12-24 15:32:36 -0500

    feat: implement player movement controls

--- main.lua
+++ main.lua
@@ -13,20 +13,51 @@ function love.load()
         storage = 'x',
         wall = '#',
         empty = ' ',
+        outOfBounds = '-'
     }
 
     --Initialize level
     Level = {
-        {' ', ' ', '#', '#', '#'},
-        {' ', ' ', '#', 'x', '#'},
-        {' ', ' ', '#', ' ', '#', '#', '#', '#'},
+        {'-', '-', '#', '#', '#'},
+        {'-', '-', '#', 'x', '#'},
+        {'-', '-', '#', ' ', '#', '#', '#', '#'},
         {'#', '#', '#', '$', ' ', '$', 'x', '#'},
         {'#', 'x', ' ', '$', '@', '#', '#', '#'},
         {'#', '#', '#', '#', '$', '#'},
-        {' ', ' ', ' ', '#', 'x', '#'},
-        {' ', ' ', ' ', '#', '#', '#'},
+        {'-', '-', '-', '#', 'x', '#'},
+        {'-', '-', '-', '#', '#', '#'},
     }
 
+    --Initialize player position
+    PlayerPos = {
+        x = 5,
+        y = 5,
+    }
+
+end
+
+--Player controls
+function love.keypressed(key)
+
+    --On arrow key press, move player in direction pressed
+    if key == 'up' or key == 'down' or key == 'left' or key == 'right' then
+        local dx = 0
+        local dy = 0
+        if key == 'left' then dx = -1 end
+        if key == 'right' then dx = 1 end
+        if key == 'up' then dy = -1 end
+        if key == 'down' then dy = 1 end
+
+        local nextPos = {
+            x = PlayerPos.x + dx,
+            y = PlayerPos.y + dy,
+        }
+
+        if IsValidPos(nextPos) then
+            UpdatePlayerPos(nextPos)
+        end
+
+    end
 end
 
 function love.draw()
@@ -40,12 +71,13 @@ function love.draw()
         [Cells.boxOnStorage] = {.59, 1, .5},
         [Cells.storage] = {.61, .9, 1},
         [Cells.wall] = {1, .58, .82},
+        [Cells.empty] = {1, 1, 1},
     }
 
     --Draw level
     for y, row in ipairs(Level) do
         for x, cell in ipairs(row) do
-            if cell ~= Cells.empty then
+            if cell ~= Cells.outOfBounds then
 
                 --Draw cell bg
                 local color = colors[cell]
@@ -59,6 +91,7 @@ function love.draw()
                 )
                 --Draw cell text
                 love.graphics.setColor(1, 1, 1)
+                local value = ''
                 love.graphics.print(
                     Level[y][x], --value
                     (x - 1) * cellSize, --x pos
@@ -67,4 +100,32 @@ function love.draw()
             end
         end
     end
+end
+
+--Determine if player can move to a position
+function IsValidPos(pos)
+    local x = pos.x
+    local y = pos.y
+
+    --Prevent movement out of bounds
+    if y < 1 or y > #Level then return false end
+    if x < 1 or x > #Level[y] then return false end
+
+    --Prevent movement to non-empty cells
+    local cell = Level[pos.y][pos.x]
+    if cell ~= Cells.empty then return false end
+
+    return true
+end
+
+--Move player to new position and update position vars
+function UpdatePlayerPos(pos)
+
+    --Update position
+    local prevPos = PlayerPos
+    PlayerPos = pos
+
+    --Update cells
+    Level[prevPos.y][prevPos.x] = Cells.empty
+    Level[PlayerPos.y][PlayerPos.x] = Cells.player
 end
