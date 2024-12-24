c1fdbd6eadf7da36f24b2436d3f4926114fb67a0
Lucas Schraier <lucas.schraier@shipsigma.com>
2024-12-24 16:21:13 -0500

    feat: implement player movement onto storage cells

--- main.lua
+++ main.lua
@@ -13,26 +13,48 @@ function love.load()
         storage = 'x',
         wall = '#',
         empty = ' ',
-        outOfBounds = '-'
+        outOfBounds = '-',
+    }
+    CellSize = 23
+    CellColors = {
+        [Cells.player] = {.64, .53, 1},
+        [Cells.playerOnStorage] = {.62, .47, 1},
+        [Cells.box] = {1, .79, .49},
+        [Cells.boxOnStorage] = {.59, 1, .5},
+        [Cells.storage] = {.61, .9, 1},
+        [Cells.wall] = {1, .58, .82},
+        [Cells.empty] = {1, 1, 1},
     }
 
     --Initialize level
     Level = {
-        {'-', '-', '#', '#', '#'},
-        {'-', '-', '#', 'x', '#'},
-        {'-', '-', '#', ' ', '#', '#', '#', '#'},
-        {'#', '#', '#', '$', ' ', '$', 'x', '#'},
-        {'#', 'x', ' ', '$', '@', '#', '#', '#'},
-        {'#', '#', '#', '#', '$', '#'},
-        {'-', '-', '-', '#', 'x', '#'},
-        {'-', '-', '-', '#', '#', '#'},
+        {'#', '#', '#', '#', '#'},
+        {'#', '@', ' ', 'x', '#'},
+        {'#', ' ', '$', ' ', '#'},
+        {'#', 'x', '$', ' ', '#'},
+        {'#', ' ', '$', 'x', '#'},
+        {'#', 'x', '$', 'x', '#'},
+        {'#', 'x', '*', ' ', '#'},
+        {'#', ' ', '*', 'x', '#'},
+        {'#', ' ', '*', ' ', '#'},
+        {'#', 'x', '*', 'x', '#'},
+        {'#', '#', '#', '#', '#'},
     }
 
-    --Initialize player position
     PlayerPos = {
-        x = 5,
-        y = 5,
+        x = 0,
+        y = 0,
     }
+    --Initialize player position
+    for y, row in ipairs(Level) do
+        for x, cell in ipairs(row) do
+            if cell == Cells.player or cell == Cells.playerOnStorage then
+                PlayerPos.x = x
+                PlayerPos.y = y
+                break
+            end
+        end
+    end
 
 end
 
@@ -62,44 +84,40 @@ end
 
 function love.draw()
 
-    local cellSize = 23
-
-    local colors = {
-        [Cells.player] = {.64, .53, 1},
-        [Cells.playerOnStorage] = {.62, .47, 1},
-        [Cells.box] = {1, .79, .49},
-        [Cells.boxOnStorage] = {.59, 1, .5},
-        [Cells.storage] = {.61, .9, 1},
-        [Cells.wall] = {1, .58, .82},
-        [Cells.empty] = {1, 1, 1},
-    }
-
     --Draw level
     for y, row in ipairs(Level) do
         for x, cell in ipairs(row) do
             if cell ~= Cells.outOfBounds then
-
-                --Draw cell bg
-                local color = colors[cell]
-                love.graphics.setColor(color)
-                love.graphics.rectangle(
-                    'fill',
-                    (x - 1) * cellSize, --x pos
-                    (y - 1) * cellSize, --y pos
-                    cellSize, --width
-                    cellSize --height
-                )
-                --Draw cell text
-                love.graphics.setColor(1, 1, 1)
-                local value = ''
-                love.graphics.print(
-                    Level[y][x], --value
-                    (x - 1) * cellSize, --x pos
-                    (y - 1) * cellSize --y pos
-                )
+                DrawCell(x, y, cell)
             end
         end
     end
+end
+
+function DrawCell(tableX, tableY, cell)
+    --Calc graphics position based on table coords
+    local x = (tableX - 1) * CellSize
+    local y = (tableY - 1) * CellSize
+
+    --Draw cell bg
+    local color = CellColors[cell]
+    love.graphics.setColor(color)
+    love.graphics.rectangle(
+        'fill',
+        x, --x pos
+        y, --y pos
+        CellSize, --width
+        CellSize --height
+    )
+
+    --Draw cell text
+    love.graphics.setColor(1, 1, 1)
+    local value = ''
+    love.graphics.print(
+        Level[tableY][tableX], --value
+        x, --x pos
+        y --y pos
+    )
 end
 
 --Determine if player can move to a position
@@ -113,9 +131,9 @@ function IsValidPos(pos)
 
     --Prevent movement to non-empty cells
     local cell = Level[pos.y][pos.x]
-    if cell ~= Cells.empty then return false end
+    if cell == Cells.empty or cell == Cells.storage then return true end
 
-    return true
+    return false
 end
 
 --Move player to new position and update position vars
@@ -123,9 +141,25 @@ function UpdatePlayerPos(pos)
 
     --Update position
     local prevPos = PlayerPos
+    local prevCell = Level[prevPos.y][prevPos.x]
     PlayerPos = pos
 
-    --Update cells
-    Level[prevPos.y][prevPos.x] = Cells.empty
-    Level[PlayerPos.y][PlayerPos.x] = Cells.player
+    --Update cell being moved to
+    local nextCell = Level[pos.y][pos.x]
+    --If player is moving onto storage, update to player on storage char
+    if nextCell == Cells.storage then
+        Level[pos.y][pos.x] = Cells.playerOnStorage
+    --Else update to player char
+    else
+        Level[pos.y][pos.x] = Cells.player
+    end
+
+    --Update cell being moved from
+    --If player was on storage, update prev cell to storage
+    if prevCell == Cells.playerOnStorage then
+        Level[prevPos.y][prevPos.x] = Cells.storage
+    --Else update prev cell to empty
+    else
+        Level[prevPos.y][prevPos.x] = Cells.empty
+    end
 end
